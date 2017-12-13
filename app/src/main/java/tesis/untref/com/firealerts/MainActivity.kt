b6a8ddb8fec398ac31fb64068efbabd7523432be
Matias Mariani <matias@Matiass-MacBook-Pro.local>
2017-12-13 12:26:32 -0300

    first commit: empty project

---
+++ app/src/main/java/tesis/untref/com/firealerts/MainActivity.kt
@@ -0,0 +1,14 @@
+package tesis.untref.com.firealerts
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+//https://antonioleiva.com/mvp-android/
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
