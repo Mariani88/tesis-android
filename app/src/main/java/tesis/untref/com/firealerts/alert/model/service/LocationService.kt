20113d4eb0e3de992ce2e44bd875e91317f3252a
Mariani88 <pm_mariani@hotmail.com>
2018-03-02 23:25:02 -0300

    add tests for locationService

--- app/src/main/java/tesis/untref/com/firealerts/alert/model/service/LocationService.kt
+++ app/src/main/java/tesis/untref/com/firealerts/alert/model/service/LocationService.kt
@@ -3,16 +3,33 @@ package tesis.untref.com.firealerts.alert.model.service
 import android.location.Address
 import com.google.android.gms.maps.model.LatLng
 import android.location.Geocoder
+import android.util.Log
 import io.reactivex.Maybe
 import io.reactivex.Single
 import tesis.untref.com.firealerts.extensions.emptyAddress
 
-class LocationService (private val geoCoder: Geocoder){
+class LocationService(private val geoCoder: Geocoder) {
+
+    companion object {
+        val EMPTY_ADDRESS = mutableListOf(emptyAddress())
+        const val MAX_RESULTS = 1
+    }
 
     fun obtainAddress(latLng: LatLng): Single<Address> =
-        Maybe
-                .just(geoCoder.getFromLocation(latLng.latitude, latLng.longitude, 1))
-                .onErrorReturn { listOf(emptyAddress()) }
-                .map { it[0] }
-                .toSingle()
+            Maybe
+                    .just(getAddress(latLng))
+                    .onErrorReturnItem(EMPTY_ADDRESS)
+                    .defaultIfEmpty(EMPTY_ADDRESS)
+                    .map { returnSpecificAddress(it) }
+                    .toSingle()
+
+    private fun getAddress(latLng: LatLng): MutableList<Address> = try {
+        geoCoder.getFromLocation(latLng.latitude, latLng.longitude, MAX_RESULTS)
+    }catch (e: Exception){
+        Log.d("error on location", "error on get address. Check network")
+        EMPTY_ADDRESS
+    }
+
+    private fun returnSpecificAddress(addresses: List<Address>): Address =
+            if (addresses.isEmpty()) EMPTY_ADDRESS[0] else addresses[0]
 }
---
+++ app/src/test/java/tesis/untref/com/firealerts/unit/alert/model/LocationServiceTest.kt
@@ -0,0 +1,105 @@
+package tesis.untref.com.firealerts.unit.alert.model
+
+import android.location.Address
+import android.location.Geocoder
+import com.google.android.gms.maps.model.LatLng
+import junit.framework.Assert
+import junit.framework.Assert.fail
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import tesis.untref.com.firealerts.alert.model.service.LocationService
+import tesis.untref.com.firealerts.extensions.emptyAddress
+import java.io.IOException
+
+class LocationServiceTest {
+
+    private val latLng = LatLng(30.0, 30.0)
+    private lateinit var locationService: LocationService
+    private var addresses: MutableList<Address>? = null
+
+    @Mock
+    private lateinit var validAddress: Address
+
+    @Mock
+    private lateinit var geoCoder: Geocoder
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        Mockito.`when`(validAddress.getAddressLine(0)).thenReturn("valid address")
+        locationService = LocationService(geoCoder)
+    }
+
+    @Test
+    fun unavailableNetworkReturnEmptyAddress(){
+        givenGeoCoderWithUnavailableNetwork()
+
+        whenFindLocation()
+
+        thenReturnEmptyAddress()
+    }
+
+    @Test
+    fun addressesNotFoundReturnEmptyAddress(){
+        givenGeoCoderWithEmptyAddressList()
+
+        whenFindLocation()
+
+        thenReturnEmptyAddress()
+    }
+
+    @Test
+    fun ifGeocoderReturnNullThenReturnEmptyAddress(){
+        givenGeoCoderReturnNullList()
+
+        whenFindLocation()
+
+        thenReturnEmptyAddress()
+    }
+
+    @Test
+    fun ifGeocoderReturnValidAddressThenReturnAddress(){
+        givenGeoCoderReturnValidAddress()
+
+        whenFindLocation()
+
+        thenReturnValidAddress()
+    }
+
+    private fun thenReturnValidAddress() {
+        Assert.assertTrue(addresses!!.isNotEmpty())
+        Assert.assertEquals(validAddress.getAddressLine(0), addresses!![0].getAddressLine(0))
+    }
+
+    private fun givenGeoCoderReturnValidAddress() {
+        Mockito.`when`(geoCoder.getFromLocation(latLng.latitude, latLng.longitude, 1))
+                .thenReturn(mutableListOf(validAddress))
+    }
+
+    private fun givenGeoCoderReturnNullList() {
+        Mockito.`when`(geoCoder.getFromLocation(latLng.latitude, latLng.longitude, 1))
+                .thenReturn(null)
+    }
+
+    private fun thenReturnEmptyAddress() {
+        Assert.assertTrue(addresses!!.isNotEmpty())
+        Assert.assertEquals(emptyAddress().getAddressLine(0), addresses!![0].getAddressLine(0))
+    }
+
+    private fun whenFindLocation() {
+        addresses = locationService.obtainAddress(latLng).test().await().values()
+    }
+
+    private fun givenGeoCoderWithUnavailableNetwork() {
+        Mockito.`when`(geoCoder.getFromLocation(latLng.latitude, latLng.longitude, 1))
+                .thenThrow(IOException())
+    }
+
+    private fun givenGeoCoderWithEmptyAddressList() {
+        Mockito.`when`(geoCoder.getFromLocation(latLng.latitude, latLng.longitude, 1))
+                .thenReturn(mutableListOf())
+    }
+}
