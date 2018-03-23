15e908b31c29ccf31cd622d44697bad951062a04
Matias Mariani <matias@Matiass-MacBook-Pro.local>
2018-03-23 14:37:32 -0300

    refactor on local notification service

--- app/src/main/java/tesis/untref/com/firealerts/alert/infrastructure/notification/AlertLocalNotificationService.kt
+++ app/src/main/java/tesis/untref/com/firealerts/alert/infrastructure/notification/AlertLocalNotificationService.kt
@@ -11,53 +11,43 @@ import android.app.PendingIntent
 import android.net.Uri
 import android.os.Vibrator
 import android.support.v4.app.NotificationCompat.VISIBILITY_PUBLIC
-import android.media.RingtoneManager
-import android.media.Ringtone
-
-
-
 
 class AlertLocalNotificationService(private val context: Context, private val random: Random) {
 
     fun sendNotification() {
-
-        val notifyIntent = Intent(context, MainActivity::class.java)
-
-        notifyIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-
-        val notifyPendingIntent = PendingIntent.getActivity(
-                context, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
-        )
-        
-
-
-
-
         val notificationBuilder = NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.ic_stat_alert_notification)
                 .setContentTitle("ALERTA DE INCENDIO")
                 .setContentText("Obtén la ubicación ingrensando a la app")
-                .setContentIntent(notifyPendingIntent)
+                .setContentIntent(createPendingIntent())
                 .setAutoCancel(true)
                 .setVibrate(longArrayOf(500, 1000, 500, 1000))
                 .setSound(getSoundUri())
                 .setPriority(NotificationCompat.PRIORITY_MAX)
                 .setVisibility(VISIBILITY_PUBLIC)
+        sendNotification(notificationBuilder)
+        vibrate()
+    }
 
-
-
-
-        val notificationId = generateId()
-        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(notificationId, notificationBuilder.build())
-
+    private fun vibrate() {
         val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         vibrator.vibrate(longArrayOf(500, 1000, 500, 1000), -1)
     }
 
+    private fun sendNotification(notificationBuilder: NotificationCompat.Builder) {
+        val notificationId = generateId()
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(notificationId, notificationBuilder.build())
+    }
 
-    private fun getSoundUri(): Uri{
-       return Uri.parse("android.resource://"+ getPackageName() +"/raw/"+  R.raw.alarm)
+    private fun createPendingIntent(): PendingIntent {
+        val notifyIntent = Intent(context, MainActivity::class.java)
+        notifyIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        return PendingIntent.getActivity(context, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
+
+    private fun getSoundUri(): Uri {
+        return Uri.parse("android.resource://" + getPackageName() + "/raw/" + R.raw.alarm)
     }
 
     private fun getPackageName() = "tesis.untref.com.firealerts"
